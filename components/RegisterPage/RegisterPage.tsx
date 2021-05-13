b6a7f99b326dc8b2bc5088438c081b21086f2cc3
Young Joo Kim <feel2174@gmail.com>
2021-05-13 21:52:16 +0900

    feat: added create User using firebase auth

--- components/RegisterPage/RegisterPage.tsx
+++ components/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,9 @@
 import { useForm } from 'react-hook-form';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { useRef, useState } from 'react';
+import firebase from '../../firebase';
+import md5 from 'md5';
 
 const RegisterPage = () => {
   const {
@@ -9,11 +12,36 @@ const RegisterPage = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+  const [errorSubmit, setErrorSubmit] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  console.log(watch('example'));
+  const onSubmit = async (data) => {
+    try {
+      const createdUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(data.email, data.password);
+      console.log('createdUser', createdUser);
+
+      await createdUser.user?.updateProfile({
+        displayName: data.name,
+        photoURL: `http://gravatar.com/avatar/${md5(
+          createdUser.user.email,
+        )}?d=identicon`,
+      });
+
+      setLoading(false);
+    } catch (error) {
+      setErrorSubmit(error.message);
+      setLoading(false);
+      setTimeout(() => {
+        setErrorSubmit('');
+      }, 3000);
+    }
+  };
+  const password = useRef();
+  password.current = watch('password');
+
+  // console.log(watch('email'));
 
   return (
     <div className="auth-wrapper">
@@ -22,20 +50,35 @@ const RegisterPage = () => {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Email</label>
-        <input type="email" defaultValue="email" {...register('email')} />
+        <input
+          type="email"
+          defaultValue="email"
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <span>This field is for Email</span>}
         <label>Name</label>
-        <input {...register('name', { required: true })} />
+        <input {...register('name', { required: true, maxLength: 10 })} />
         <label>Password</label>
-        <input type="password" {...register('password', { required: true })} />
+        <input
+          type="password"
+          {...register('password', { required: true, minLength: 8 })}
+        />
+        {errors.password && <span>password has to be over 8 characters</span>}
         <label>Password Confirm</label>
         <input
           type="password"
-          {...register('password_confirm', { required: true })}
+          {...register('password_confirm', {
+            required: true,
+            validate: (value) => value === password.current,
+          })}
         />
+        {errors.password_confirm &&
+          errors.password_confirm.type === 'validate' && (
+            <p>The passwords do not match</p>
+          )}
+        {errorSubmit && <p>{errorSubmit}</p>}
 
-        {/* {errors.exampleRequired && <span>This field is required</span>} */}
-
-        <input type="submit" />
+        <input type="submit" disabled={loading} />
         <LoginLink href="/login">이미 아이디가 있다면...</LoginLink>
       </form>
     </div>
